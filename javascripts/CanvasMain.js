fd9c0a4ecca1deb3ac20ba7ab27a60bd39dbb06d
PetaroMitaro <mitranopeter@gmail.com>
2014-11-29 21:33:12 -0500

    attemping to include ipad support

--- javascripts/CanvasMain.js
+++ javascripts/CanvasMain.js
@@ -15,7 +15,50 @@ var _rightSide;
 var leftWeight;
 var rightWeight;
 
+
+
 function init() {
+
+	$.fn.addTouch = function(){
+	  this.each(function(i,el){
+	    $(el).bind('touchstart touchmove touchend touchcancel',function(){
+	      //we pass the original event object because the jQuery event
+	      //object is normalized to w3c specs and does not provide the TouchList
+	      handleTouch(event);
+	    });
+	  });
+	 
+	  var handleTouch = function(event)
+	  {
+	    var touches = event.changedTouches,
+	            first = touches[0],
+	            type = '';
+	 
+	    switch(event.type)
+	    {
+	      case 'touchstart':
+	        type = 'mousedown';
+	        break;
+	 
+	      case 'touchmove':
+	        type = 'mousemove';
+	        event.preventDefault();
+	        break;
+	 
+	      case 'touchend':
+	        type = 'mouseup';
+	        break;
+	 
+	      default:
+	        return;
+	    }
+	 
+	    var simulatedEvent = document.createEvent('MouseEvent');
+	    simulatedEvent.initMouseEvent(type, true, true, window, 1, first.screenX, first.screenY, first.clientX, first.clientY, false, false, false, false, 0/*left*/, null);
+	    first.target.dispatchEvent(simulatedEvent);
+	  };
+	};
+
 
 	_termRack = $("#new_term_rack");
 	var termRackHeight = _termRack.clientHeight;
